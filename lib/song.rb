74737bd56c84ac22d936490413a37b03fd35de45
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-01-28 14:14:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,15 @@ class Song
   
   def self.genre_count
     genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   end 
     
   def self.artist_count
